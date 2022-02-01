b4a6589e0bb50f4c5d8e117e336f28760175d608
paireks <47977819+paireks@users.noreply.github.com>
2022-02-01 18:52:27 +0100

    Add Meshes

--- test/ToolboxForTests.cs
+++ test/ToolboxForTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using dotbim;
 
 namespace test
@@ -26,6 +27,76 @@ namespace test
                    Math.Abs(rotationToCheck.Qy - rotationExpected.qy) < tolerance &&
                    Math.Abs(rotationToCheck.Qz - rotationExpected.qz) < tolerance &&
                    Math.Abs(rotationToCheck.Qw - rotationExpected.qw) < tolerance;
+        }
+        
+        public static Mesh CreateTestTriangleMesh()
+        {
+            List<double> verticesCoordinates = new List<double>
+            {
+                0.0, 0.0, 0.0,
+                10.0, 0.0, 0.0,
+                10.0, -15.0, 0.0
+            };
+
+            List<int> facesIds = new List<int>
+            {
+                0,1,2
+            };
+
+            Mesh mesh = new Mesh
+            {
+                MeshId = 12,
+                VerticesCoordinates = verticesCoordinates,
+                FacesIds = facesIds,
+            };
+
+            return mesh;
+        }
+
+        public static Mesh CreateTestCubeMesh()
+        {
+            List<double> verticesCoordinates = new List<double>
+            {
+                0.0, 0.0, 0.0,
+                10.0, 0.0, 0.0,
+                10.0, 0.0, 20.0,
+                0.0, 0.0, 20.0,
+                0.0, 30.0, 0.0,
+                10.0, 30.0, 0.0,
+                10.0, 30.0, 20.0,
+                0.0, 30.0, 20.0
+            };
+            
+            List<int> facesIds = new List<int>
+            {
+                // Front side
+                1, 2, 3,
+                1, 3, 4,
+                
+                // Bottom side
+                1, 2, 5,
+                2, 5, 6,
+                
+                // Left side
+                1, 5, 4,
+                5, 4, 8,
+                
+                // Right side
+                2, 3, 6,
+                3, 6, 7,
+                
+                // Top side
+                3, 4, 8,
+                3, 7, 8,
+                
+                // Back side
+                5, 6, 8,
+                6, 7, 8
+            };
+
+            Mesh mesh = new Mesh{VerticesCoordinates = verticesCoordinates, FacesIds = facesIds, MeshId = 0};
+
+            return mesh;
         }
     }
 }
---
+++ test/UnitTests/TestMesh.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using dotbim;
+using Xunit;
+
+namespace test.UnitTests
+{
+    public class TestMesh
+    {
+        #region Triangle
+
+        [Fact]
+        public void TestMesh_Triangle_MeshId()
+        {
+            var mesh = ToolboxForTests.CreateTestTriangleMesh();
+            
+            Assert.Equal(12, mesh.MeshId);
+        }
+
+        [Fact]
+        public void TestMesh_Triangle_VerticesCoordinates()
+        {
+            var mesh = ToolboxForTests.CreateTestTriangleMesh();
+            
+            List<double> expected = new List<double>
+            {
+                0,0,0,10,0,0,10,-15,0
+            };
+            
+            Assert.Equal(expected, mesh.VerticesCoordinates);
+        }
+
+        [Fact]
+        public void TestMesh_Triangle_FacesIds()
+        {
+            var mesh = ToolboxForTests.CreateTestTriangleMesh();
+            
+            List<int> expected = new List<int>
+            {
+                0,1,2
+            };
+            
+            Assert.Equal(expected, mesh.FacesIds);
+        }
+
+        #endregion
+
+        #region Cube
+
+        [Fact]
+        public void TestMesh_Cube_MeshId()
+        {
+            var mesh = ToolboxForTests.CreateTestCubeMesh();
+            
+            Assert.Equal(0, mesh.MeshId);
+        }
+
+        [Fact]
+        public void TestMesh_Cube_VerticesCoordinates()
+        {
+            var mesh = ToolboxForTests.CreateTestCubeMesh();
+            
+            List<double> expected = new List<double>
+            {
+                0.0, 0.0, 0.0,
+                10.0, 0.0, 0.0,
+                10.0, 0.0, 20.0,
+                0.0, 0.0, 20.0,
+                0.0, 30.0, 0.0,
+                10.0, 30.0, 0.0,
+                10.0, 30.0, 20.0,
+                0.0, 30.0, 20.0
+            };
+            
+            Assert.Equal(expected, mesh.VerticesCoordinates);
+        }
+
+        [Fact]
+        public void TestMesh_Cube_FacesIds()
+        {
+            var mesh = ToolboxForTests.CreateTestCubeMesh();
+            
+            List<int> expected = new List<int>
+            {
+                // Front side
+                1, 2, 3,
+                1, 3, 4,
+                
+                // Bottom side
+                1, 2, 5,
+                2, 5, 6,
+                
+                // Left side
+                1, 5, 4,
+                5, 4, 8,
+                
+                // Right side
+                2, 3, 6,
+                3, 6, 7,
+                
+                // Top side
+                3, 4, 8,
+                3, 7, 8,
+                
+                // Back side
+                5, 6, 8,
+                6, 7, 8
+            };
+            
+            Assert.Equal(expected, mesh.FacesIds);
+        }
+
+        #endregion
+
+        #region Exceptions
+
+        [Fact]
+        public void TestColor_Incorrect_ThrowsException()
+        {
+            var exception = Assert.Throws<ArgumentException>(() => new Mesh
+            {
+                MeshId = -1,
+                FacesIds = new List<int>{0,1,2},
+                VerticesCoordinates = new List<double>
+                {
+                    0.0,0.0,0.0,
+                    10.0,0.0,0.0,
+                    10.0,5.0,0.0
+                }
+            });
+            Assert.Equal("MeshId should be >= 0", exception.Message);
+        }   
+
+        #endregion
+    }
+}
