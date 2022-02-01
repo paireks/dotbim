97c61cb1744346d939f6ffb75cfb022325c9d85a
paireks <47977819+paireks@users.noreply.github.com>
2022-02-01 19:46:31 +0100

    Start of File

---
+++ dotbim/File.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace dotbim
+{
+    [Serializable]
+    [JsonObject("file")]
+    public class File
+    {
+        public void Save(string path)
+        {
+            if (!path.EndsWith(".bim"))
+            {
+                throw new ArgumentException("Path should end up with .bim");
+            }
+
+            using (StreamWriter file = System.IO.File.CreateText(path))
+            {
+                JsonSerializer serializer = new JsonSerializer
+                {
+                    Formatting = Formatting.Indented
+                };
+                serializer.Serialize(file, this);
+            }
+        }
+
+        public static File Read(string path)
+        {
+            if (!path.EndsWith(".bim"))
+            {
+                throw new ArgumentException("Path should end up with .bim");
+            }
+
+            using (StreamReader file = System.IO.File.OpenText(path))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                return (File) serializer.Deserialize(file, typeof(File));
+            }
+        }
+
+        [JsonProperty("schema_version")] 
+        public string SchemaVersion { get; set; }
+
+        [JsonProperty("info")] 
+        public Info Info { get; set; }
+
+        [JsonProperty("meshes")] 
+        public List<Mesh> Meshes { get; set; }
+
+        [JsonProperty("elements")] 
+        public List<Element> Elements { get; set; }
+    }
+}
